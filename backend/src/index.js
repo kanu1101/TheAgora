f598a38a8f7777226a4f4b3b1dd770a9ac663da7
kanu1101 <202201261@daiict.ac.in>
2025-06-24 18:22:43 +0530

    mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
-    console.log("server is running");   
+    console.log("server is running");  
+    connectDB(); 
 })
